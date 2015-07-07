dd801e79dfa9ae5d5b8e0ace456efccf330bfc69
coreyjbrown <coreyjbrown@gmail.com>
2015-07-07 10:27:20 -0400

    Add input of form data into database

--- SupplyRequests.js
+++ SupplyRequests.js
@@ -1,8 +1,28 @@
+var dbString = '//ga-sql-bootcamp:demodb0615/Supply_Requests';
+var requestColumns = 'request_id,full_name,item,qty,priority,description,date,status,username';
+
+
+
 function addRequest(formObj){
+  var nextId = PropertiesService.getScriptProperties().getProperty('nextReqId')
   var request = formObj;
   request.date = new Date();
   request.username = Session.getActiveUser().getEmail();
+  request.id = "AMS4_" + nextId.toString();
+  request.status = "New";
   
-  NVGAS.insertSqlRecord(dbString, queryArray);
+  var query = 'INSERT INTO Requests(' + requestColumns + ') values("' 
+            + request.id + '", "'
+            + request.fullName + '","'
+            + request.item + '", "'
+            + request.qty + '", "'
+            + request.priority + '", "'
+            + request.description + '", "'
+            + request.date + '", "'
+            + request.status + '", "'
+            + request.username + '")';
+  
+  NVGAS.insertSqlRecord(dbString, [query]);
+  PropertiesService.getScriptProperties().setProperty('nextReqId', (Number(nextId) + 1).toString())
   Logger.log(request);
 }
